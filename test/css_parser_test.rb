3fd736423e1ae2dc36e8a2615d86d19901a9187f
Ron Green <rgreenjr@gmail.com>
2009-12-11 11:40:13 -0600

    refactored tests

--- test/css_parser_test.rb
+++ test/css_parser_test.rb
@@ -7,6 +7,9 @@ class CssParserTest < Test::Unit::TestCase
 
   def test_parsing
     assert_not_nil CssParser.new.parse(IO.read('test/test1.css'))
+    assert_not_nil CssParser.new.parse(IO.read('test/test2.css'))
+    assert_not_nil CssParser.new.parse(IO.read('test/test3.css'))
+    assert_not_nil CssParser.new.parse(IO.read('test/test4.css'))
   end
 
 end
